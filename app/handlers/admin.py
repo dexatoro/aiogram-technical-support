a50bbd4faa4f72bfb5809151f3a5f0c030cfffb7
dexatoro <daniyazgit@gmail.com>
2024-05-18 11:27:27 +0300

    18.05.2024

---
+++ app/handlers/admin.py
@@ -0,0 +1,44 @@
+from aiogram import Bot, Router, F
+from aiogram.filters.command import Command
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters import Filter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from app.database.requests import delete_ticket, get_ticket, get_user
+from app.keyboards import all_tickets
+
+admin = Router()
+
+class Answer(StatesGroup):
+    answer = State()
+
+
+class Admin(Filter):
+    def __init__(self):
+        self.admins = [864277871]
+        
+    async def __call__(self, message: Message):
+        return message.from_user.id in self.admins
+
+@admin.message(Admin(), Command('tickets'))
+async def tickets(message: Message):
+    await message.answer('Список всех тикетов',
+                         reply_markup=await all_tickets())
+    
+@admin.callback_query(F.data.startswith('ticket_'))
+async def answer_ticket(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(Answer.answer)
+    await callback.answer('Вы выбрали тикет')
+    ticket = await get_ticket(callback.data.split('_')[1])
+    user = await get_user(ticket.user)
+    await state.update_data(tg_id=user.tg_id)
+    await state.update_data(ticket_id=ticket.id)
+    await callback.message.answer(f'Вопрос: {ticket.text}\n{user.name} | {user.number} | {user.username}\nНапишите ответ')
+    
+@admin.message(Admin(), Answer.answer)
+async def send_answer(message: Message, state: FSMContext, bot: Bot):
+    info = await state.get_data()
+    await bot.send_message(chat_id=info['tg_id'], text=message.text)
+    await delete_ticket(info['ticket_id'])
+    await message.answer('Сообщение отправлено!')
+    await state.clear()
---
+++ app/keyboards.py
@@ -0,0 +1,19 @@
+from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardButton, KeyboardButton
+from app.database.requests import get_tickets
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+send_number = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Отправить свой номер телефона', request_contact=True)]],
+                    resize_keyboard=True,
+                    input_field_placeholder='Отправьте телефон по кнопке ниже')
+
+async def all_tickets():
+    tickets = await get_tickets()
+    keyboard = InlineKeyboardBuilder()
+    for ticket in tickets:
+        keyboard.add(InlineKeyboardButton(text=f'Тикет № {ticket.id}',
+                                           callback_data=f'ticket_{ticket.id}'))
+        return keyboard.adjust(2).as_markup()
+    
+new_ticket = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Новый запрос')]], resize_keyboard=True)
